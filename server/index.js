ed70cce4359e6ee070410ea4370df2ca6a1465cc
Guest0131 <shadow7en@ya.ru>
2022-12-16 03:02:02 +0300

    Настроены базовые настройки Express

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from  'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 
 const app = express();
 dotenv.config();
+mongoose.set('strictQuery', true);
 
 // Constants
 const APP_PORT = process.env.APP_PORT || 3002;
@@ -13,13 +15,25 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'All is fine'
+    })
+});
+
 
 async function start () {
     try {
         await mongoose.connect(
-            `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
+            `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+            { useNewUrlParser: true }
             );
-        console.log(`Server started on port: ${APP_PORT}`);
+
+        app.listen(APP_PORT, () => console.log(`Server started on port: ${APP_PORT}`))
     } catch (err) {
         console.log("Error connecting to MongoDB: ", err);
     }
